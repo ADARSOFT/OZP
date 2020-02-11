d60fee7c930135ae7958614117de4c3d22b003c6
Adarsoft <50028092+ADARSOFT@users.noreply.github.com>
2020-02-11 01:52:41 +0100

    Work on descriptive statistics, data exploratory

--- Spyder/BankMarketingAnalisys.py
+++ Spyder/BankMarketingAnalisys.py
@@ -1,34 +1,49 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Feb  9 17:31:49 2020
-
-@author: Damir
-"""
 import numpy as np
 import pandas as pd 
 from sklearn.preprocessing import LabelEncoder
 
-# 1. Read data from file (Bank marketing)
+#%% 1. Read source data from file, and separate as categorical and numerical data
 bank_marketing = pd.read_csv("../Data/master-data/bank-additional.csv", sep = ";")
 
-# 2. Show descriptive statistics for dataset (datatypes, values range, missing values)
-bank_marketing_desc = bank_marketing.describe()
+categoric_columns = ["job", "marital", "education", "default", "housing", "loan", "contact", "month", "day_of_week", "poutcome"]
+# don't select duration column, because of high corellation
+numeric_columns = ["age", "duration", "campaign", "pdays", "previous", "emp.var.rate", "cons.price.idx", "cons.conf.idx", "euribor3m", "nr.employed"]
+label_column = "y"
+label_codes = {'yes':1, 'no':0}
 
-label = bank_marketing["y"]
+# Map from yes/no to 1/0
+bank_marketing[label_column] = bank_marketing[label_column].map(label_codes)
+
+# Separate source data
+label = bank_marketing[label_column]
+categoric_data = bank_marketing.loc[:, categoric_columns].copy()
+numeric_data = bank_marketing.loc[:, numeric_columns].copy()
+
+#%% 2. Descriptive statistics for dataset (datatypes, values range, missing values)
+
+# Unique values
+numeric_data.nunique()
+categoric_data.nunique()
+
+numeric_data['pdays'].value_counts()
+	
+# Datatypes
+numeric_data.info()
+categoric_data.info()
+
+# Statistical measurements
+numeric_data.describe()
 label.describe()
 
-# Separate categoric and numeric data
-categoric_columns = ["job", "maritial", "education", "default", "housing", "loan", "contract", "month", "day_of_week", "poutcome"]
-numeric_columns = ["age", "duration", "campaign", "pdays", "previous", "emp.var.rate", "cons.price.idx", "cons.conf.idx", "euribor3m", "nr.employed"]
+# Output variable balance
+label.value_counts() / len(label)
 
-categoric_data = bank_marketing.loc[:, categoric_columns]
-
-numeric_data = bank_marketing.loc[:, numeric_columns]
-numeric_data_description = numeric_data.describe()
-
-# Check missing data
+# Missing values
 categoric_missing_columns = categoric_data.isnull().any()
 numeric_missing_columns = numeric_data.isnull().any()
 
-# 3. Prepare dataset for predictive modeling
+#%% 3. Prepare dataset for predictive modeling
+# Delete column 
+
+
 
