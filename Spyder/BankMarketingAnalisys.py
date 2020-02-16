8ef62f233004b62dd04acdf2c10619e2ee8533ea
Adarsoft <50028092+ADARSOFT@users.noreply.github.com>
2020-02-16 22:53:24 +0100

    no message

--- Spyder/BankMarketingAnalisys.py
+++ Spyder/BankMarketingAnalisys.py
@@ -19,14 +19,21 @@ label = bank_marketing[label_column]
 categoric_data = bank_marketing.loc[:, categoric_columns].copy()
 numeric_data = bank_marketing.loc[:, numeric_columns].copy()
 
+# Replace 999 with -1 for column pdays, for experiment purpose i will try with -1 and 999 both combinations
+numeric_data['pdays'] = numeric_data['pdays'].replace(999, -1) 
+
 #%% 2. Descriptive statistics for dataset (datatypes, values range, missing values)
 
-# Unique values
+# How meny unique values exists in features
 numeric_data.nunique()
 categoric_data.nunique()
+label.nunique()
 
 numeric_data['pdays'].value_counts()
-	
+
+# Show all possible values for categoric data 
+pd.value_counts(categoric_data.values.flatten())
+
 # Datatypes
 numeric_data.info()
 categoric_data.info()
@@ -42,7 +49,63 @@ label.value_counts() / len(label)
 categoric_missing_columns = categoric_data.isnull().any()
 numeric_missing_columns = numeric_data.isnull().any()
 
-#%% 3. Prepare dataset for predictive modeling
+## Missing values CATEGORICAL data
+
+''' Possible tactics for handling missing categorical data
+
+0. Drop rows where all columns are NaN
+1. Ignore missing data (delete)
+2. Develop model to predict missing values 
+3. Treat missing data as just another category
+4. Replace data by most frequent value (mode)
+
+1. Random versus selective loss of data 
+2. Missing by design, not asked, or not applicable
+'''
+
+# Option 1
+categoric_data = categoric_data.dropna(axis='columns', how='all')
+numeric_data = numeric_data.dropna(axis='columns', how='all')
+
+# Check percentage of unglonow categorical nad 
+categorical_data_missing_values = ['marital', 'education', 'default', 'housing', 'loan']
+data_len = len(categoric_data)
+
+unknowns_categorical_data_percentages = pd.DataFrame([], columns = ['UnknownPercentage'], index = categorical_data_missing_values)
+
+print ('Column name: Unknown values percentage')
+
+for column in categorical_data_missing_values:
+	percentage_unknown = categoric_data[column].value_counts()['unknown'] / data_len
+	unknowns_categorical_data_percentages.loc[column,:] = {'UnknownPercentage':percentage_unknown*100}
+	
+unknowns_categorical_data_percentages.head()
+
+print ('Column name: Null values percentage')
+
+for column in categoric_columns:
+	percentage_nan = categoric_data['marital'].isnull().value_counts() / data_len
+	print('{}: {}%'.format(column, percentage_nan * 100))
+
+# Column MARITAL (small amount of unknown data, strategy : replace with most fraquent value)
+
+col_marital_mode = categoric_data['marital'].mode()
+categoric_data['marital'] = categoric_data['marital'].replace('unknown', col_marital_mode.values[0])
+categoric_data['marital'].value_counts()
+
+# Column DEFAULT (thread unknown data as another category, because of high unknown values percentage). Action DO NOTHING, cause values already have code value 'unknown'
+
+# Columns HOUSING and LOAD have high corellated UNKNOWN values and the same number. Conclusion this columns are MAR (Missing At Random). Decision is to use predictive model
+
+# Column Education (threat unknown data as another category, because some people don't want to give informations about education, and that can be some pattern latter) MNAR (Missing Not At Random)
+
+## Missing values numeric data
+
+
+
+numeric_data['pdays'].value_counts() / len(numeric_data)
+
+#%% 3. Prepare dataset for predictive modeling (dummy variables)
 # Delete column 
 
 
